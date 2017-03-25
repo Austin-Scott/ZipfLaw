180da9e54d05e786445a8402b292c8cb5e676683
Austin <austinjscott@hotmail.com>
2017-03-25 17:25:40 -0500

    Fixed some bugs

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@
 #include <string>
 #include <sstream>
 #include <algorithm>
+#include <iomanip>
 
 using namespace std;
 using namespace experimental::filesystem;
@@ -73,8 +74,10 @@ string getWord(ifstream &file, locale loc) {
 }
 
 void addWord(string word, vector<Word> &words) {
-	unsigned long min = 0, max = words.size()-1, cur = 0;
-	if (max > 0) {
+	int min = 0;
+	int max = words.size() - 1;
+	int cur = 0;
+	if (words.size() > 0) {
 		while (true) {
 			cur = (max + min) / 2;
 			if (words[cur] == word) {
@@ -89,20 +92,16 @@ void addWord(string word, vector<Word> &words) {
 			}
 
 			if (max < min) {
-				if (max >= 0) {
-					words.insert(words.begin() + max, Word(word));
+				if (cur == min) {
+					words.insert(words.begin() + cur, Word(word));
 				}
-				else {
-					words.insert(words.begin(), Word(word));
-				}
-				break;
-			}
-			else if (min > max) {
-				if (max + 1 < words.size()) {
-					words.insert(words.begin() + (max + 1), Word(word));
-				}
-				else {
-					words.emplace_back(word);
+				else if (cur == max) {
+					if (cur >= words.size()-1) {
+						words.emplace_back(word);
+					}
+					else {
+						words.insert(words.begin() + (cur+1), Word(word));
+					}
 				}
 				break;
 			}
@@ -118,7 +117,7 @@ bool sortByCount(Word a, Word b) {
 		return a < b;
 	}
 	else {
-		return a.getCount() < b.getCount();
+		return a.getCount() > b.getCount();
 	}
 }
 
@@ -169,14 +168,14 @@ int main(int argc, char* argv[]) {
 			long double expected = 0;
 			long double error = 0;
 			for (int i = 0; i < words.size(); i++) {
-				outFile << (i + 1) << "," << words[i].getCharacters() << "," << words[i].getCount() << ",";
+				outFile <<setw(7)<<right<< (i + 1) << "," << setw(15) << right << words[i].getCharacters() << "," << setw(8) << right << words[i].getCount() << ",";
 				if (i == 0) {
 					firstFreq = (words[i].getCount() / (long double)totalWords);
 				}
 				freq = (words[i].getCount() / (long double)totalWords);
 				expected = (1 / (long double)(i + 1))*firstFreq;
-				error = abs((expected - freq) / expected)*100.0;
-				outFile << freq << "," << expected << "," << error << "," << endl;
+				error = abs((expected - freq) / expected)*(long double)100.0;
+				outFile << setw(10) << right << freq << "," << setw(10) << right << expected << "," << setw(10) << right << error << "," << endl;
 			}
 
 			outFile << endl << "-----End Results-----";
@@ -190,10 +189,10 @@ int main(int argc, char* argv[]) {
 	}
 	else {
 		if (argc > 0) {
-			cout << "Invalid number of arguments. Usage: \n" << argv[0] << " Directory of text files to parse\" \"name of textfile to write results to\"" << endl;
+			cout << "Invalid number of arguments. Usage: \n" << argv[0] << " \"Directory of text files to parse\" \"name of textfile to write results to\"" << endl;
 		}
 		else {
-			cout << "Invalid number of arguments. Usage: \n(name of executable) Directory of text files to parse\" \"name of textfile to write results to\"" << endl;
+			cout << "Invalid number of arguments. Usage: \n(name of executable) \"Directory of text files to parse\" \"name of textfile to write results to\"" << endl;
 		}
 	}
 	return 0;
